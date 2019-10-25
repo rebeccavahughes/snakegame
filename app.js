0b7c5a218e40fd3ea948aa455a98073a5d440256
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-10-25 10:46:49 +0100

    added collision function and create food function

--- app.js
+++ app.js
@@ -31,4 +31,41 @@ let drawModule = (function () {
         ctx.fillStyle = "blue";
         ctx.fillText(score_text, 145, h-5);
     }
+    let drawSnake = function() {
+        //Initially the body of the snake will be formed by 5 squares. 
+        let length = 4;
+        snake = [];
+
+        //using a for loop we push the 5 elements inside the array(squares).
+        //every element will have x = 0 and y will take the value of the index
+        for (i = length; i>=0; i--){
+            snake.push({x:i, y:0});
+        }
+    }
 })
+
+let createFood = function(){
+    food = {
+        //generate random numbers
+        x: Math.floor((Math.random() * 30) +1),
+        y: Math.floor((Math.random() * 30) +1)
+    }
+
+    //look at the position of the snakes body.
+    for (i=0, i>snake.length; i++){
+        let snakeX = snake[i].x;
+        let snakeY = snake[i].y;
+
+        if (food.x===snakeX || food.y === snakeY || food.y === snakeY && food.x===snakeX){
+            food.x = Math.floor((Math.random() * 30) + 1);
+        }
+    }
+}
+
+let checkCollision = function(x, y, array){
+    for(i = 0; i < array.length; i++){
+        if(array[i].x === x && array[i].y === y)
+        return true;
+    }
+    return false;
+}
